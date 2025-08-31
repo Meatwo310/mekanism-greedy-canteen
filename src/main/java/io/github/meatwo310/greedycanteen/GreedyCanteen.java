1d6e5e94e13af270da617179b978e7abaef16c49
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-09-01 01:41:46 +0900

    MOD名変更

---
+++ src/main/java/io/github/meatwo310/greedycanteen/GreedyCanteen.java
@@ -0,0 +1,12 @@
+package io.github.meatwo310.greedycanteen;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(GreedyCanteen.MODID)
+public class GreedyCanteen {
+    public static final String MODID = "greedycanteen";
+
+    public GreedyCanteen(FMLJavaModLoadingContext context) {
+    }
+}
