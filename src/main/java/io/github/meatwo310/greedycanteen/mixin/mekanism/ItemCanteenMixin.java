dc46cea91704188aaa06dcb32b62567a57921372
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-09-01 15:32:05 +0900

    ツールチップ！

--- src/main/java/io/github/meatwo310/greedycanteen/mixin/mekanism/ItemCanteenMixin.java
+++ src/main/java/io/github/meatwo310/greedycanteen/mixin/mekanism/ItemCanteenMixin.java
@@ -1,15 +1,43 @@
 package io.github.meatwo310.greedycanteen.mixin.mekanism;
 
 import com.llamalad7.mixinextras.sugar.Local;
+import mekanism.api.text.EnumColor;
+import mekanism.client.key.MekKeyHandler;
+import mekanism.client.key.MekanismKeyHandler;
+import mekanism.common.MekanismLang;
 import mekanism.common.config.MekanismConfig;
 import mekanism.common.item.gear.ItemCanteen;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
 
 @Mixin(value = ItemCanteen.class)
 public abstract class ItemCanteenMixin {
+    @Inject(method = "appendHoverText", at = @At("TAIL"))
+    private void appendHoverTextInject(@NotNull ItemStack stack, @Nullable Level world, @NotNull List<Component> tooltip, @NotNull TooltipFlag flag, CallbackInfo ci) {
+        if (MekKeyHandler.isKeyPressed(MekanismKeyHandler.detailsKey)) {
+            tooltip.add(Component.translatable("tooltip.greedycanteen.canteen.description1"));
+            tooltip.add(Component.translatable("tooltip.greedycanteen.canteen.description2"));
+        } else {
+            tooltip.add(MekanismLang.HOLD_FOR_DETAILS.translateColored(EnumColor.GRAY,
+                    EnumColor.INDIGO,
+                    MekanismKeyHandler.detailsKey.getTranslatedKeyMessage()
+            ));
+        }
+    }
+
     @Redirect(method = "finishUsingItem", at = @At(value = "INVOKE", target = "Ljava/lang/Math;min(II)I"))
     private int min(int foodNeeded, int min, @Local Player player) {
         if (player.isShiftKeyDown()) {
